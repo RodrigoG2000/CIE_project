7a34eefcc42cd253c3d50b3bc6cfa981df8c7827
RodrigoG2000 <rodgp2019@gmail.com>
2023-11-25 15:15:02 -0600

    Update App.js
    Descripcion piola

--- cie-project-reactjs/src/App.js
+++ cie-project-reactjs/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   return (
+   <div>
    <h1>Hola mundo, mi nombre es Rodrigo y este es mi primer intento con Github y no se que rayos hago</h1>
+   <p>Este es un parrafo y aqui ira el servicio de login junto con el de autenticacion</p>
+   </div>
   );
 }
 
