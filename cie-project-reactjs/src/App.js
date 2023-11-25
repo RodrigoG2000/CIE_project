4485ce1e5f464b90dc83c96093da186cf6268940
RodrigoG2000 <rodgp2019@gmail.com>
2023-11-25 15:46:24 -0600

    Update App.js

--- cie-project-reactjs/src/App.js
+++ cie-project-reactjs/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./Login";
 
 function App() {
   return (
    <div>
-    <h1>LOGIN</h1>
+    <Login/>
    </div>
   );
 }
