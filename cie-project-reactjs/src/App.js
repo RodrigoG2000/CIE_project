ef41137c76da6df0050482d9c992f5499a6afe1b
RodrigoG2000 <rodgp2019@gmail.com>
2023-11-25 15:25:29 -0600

    Update App.js
    Comenzando desde cero

--- cie-project-reactjs/src/App.js
+++ cie-project-reactjs/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
    <div>
-   <h1>Hola mundo, mi nombre es Rodrigo y este es mi primer intento con Github y no se que rayos hago</h1>
-   <p>Este es un parrafo y aqui ira el servicio de login junto con el de autenticacion</p>
+    <h1>LOGIN</h1>
    </div>
   );
 }
