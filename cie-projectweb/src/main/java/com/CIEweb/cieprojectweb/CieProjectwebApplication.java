45a64a499906ea5411b7f47ff2456fff00519536
RodrigoG2000 <rodgp2019@gmail.com>
2023-12-05 06:34:40 -0600

    Añadimos el backend

---
+++ cie-projectweb/src/main/java/com/CIEweb/cieprojectweb/CieProjectwebApplication.java
@@ -0,0 +1,13 @@
+package com.CIEweb.cieprojectweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CieProjectwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CieProjectwebApplication.class, args);
+	}
+
+}
---
+++ cie-projectweb/src/test/java/com/CIEweb/cieprojectweb/CieProjectwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CIEweb.cieprojectweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CieProjectwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
