570392c234c3090b9be804d0a0146478171a426f
RodrigoG2000 <rodgp2019@gmail.com>
2023-12-09 15:41:33 -0600

    4

--- cie-projectweb/src/main/java/com/CIEweb/cieprojectweb/CieProjectwebApplication.java
+++ cie-projectweb/src/main/java/com/CIEweb/cieprojectweb/CieProjectwebApplication.java
@@ -2,11 +2,13 @@ package com.CIEweb.cieprojectweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 public class CieProjectwebApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(CieProjectwebApplication.class, args);
 	}
 
