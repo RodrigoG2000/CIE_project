09738c8934060459676b3604e5b70fdb18ed0141
RodrigoG2000 <rodgp2019@gmail.com>
2023-12-09 13:42:18 -0600

    Responsive
    I implemented a responsive design for our website to ensure optimal viewing on various devices.

--- cie-projectweb/src/main/resources/static/cie-project-reactjs/src/App.js
+++ cie-projectweb/src/main/resources/static/cie-project-reactjs/src/App.js
@@ -7,11 +7,11 @@ import Presentation from "./components/Presentation";
 function App() {
   return (
    <div>
-      <div className="flex w-full h-screen">
-        <div className="w-full flex items-center justify-center lg:w-1/2">
+      <div className = "grid grid-cols-1 lg:grid-cols-2 w-full h-screen">
+        <div className="flex items-center justify-center lg:col-span-1">
           <Form/>
           </div>
-        <div className="hidden relative lg:flex h-full  w-1/2 items-start p-20 justify-center bg-cover bg-center" style={{ backgroundImage: `url(${fondo})` }}>
+        <div className= "hidden relative lg:flex h-full w-full items-start p-20 justify-center bg-cover bg-center" style={{ backgroundImage: `url(${fondo})` }}>
           <Presentation/>
         </div>
         </div>
